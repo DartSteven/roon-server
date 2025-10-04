0d443ca880d3b38388d4b81d248da163e7d0b908
DartSteven <dartsteven@icloud.com>
2025-10-04 17:03:32 +0200

    v0.1.1 - Small fixes and cleanup

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,40 +2,51 @@
 
 echo "Starting container."
 
-# Determine the Roon package URL based on the ROON_VERSION environment variable
-if [ "$ROON_VERSION" == "EarlyAccess" ]; then
-  ROON_PACKAGE_URI="https://download.roonlabs.net/builds/earlyaccess/RoonServer_linuxx64.tar.bz2"
+# normalize and remove CR/LF
+ROON_VERSION_RAW="${ROON_VERSION:-}"
+ROON_VERSION_CLEAN="${ROON_VERSION_RAW,,}"            # lowercase
+ROON_VERSION_CLEAN="${ROON_VERSION_CLEAN//$'\r'/}"    # remove CR
+ROON_VERSION_CLEAN="${ROON_VERSION_CLEAN//$'\n'/}"    # remove LF
+
+# allow explicit override of package URI
+if [ -n "${ROON_PACKAGE_URI}" ]; then
+  echo "Using explicit ROON_PACKAGE_URI=${ROON_PACKAGE_URI}"
 else
-  ROON_PACKAGE_URI="http://download.roonlabs.com/builds/RoonServer_linuxx64.tar.bz2"
+  if [ "${ROON_VERSION_CLEAN}" = "earlyaccess" ]; then
+    ROON_PACKAGE_URI="https://download.roonlabs.net/builds/earlyaccess/RoonServer_linuxx64.tar.bz2"
+    echo "Selected EarlyAccess branch"
+  else
+    ROON_PACKAGE_URI="http://download.roonlabs.com/builds/RoonServer_linuxx64.tar.bz2"
+    echo "Selected Stable branch"
+  fi
 fi
 
 echo "Starting RoonServer with user $(whoami)"
+echo "ROON_VERSION='${ROON_VERSION}'"
+echo "Final ROON_PACKAGE_URI='${ROON_PACKAGE_URI}'"
 
 # Install Roon if not present
 if [ ! -f /opt/RoonServer/start.sh ]; then
   echo "Downloading Roon Server from ${ROON_PACKAGE_URI}"
-  wget --tries=2 -O - ${ROON_PACKAGE_URI} | tar -xvj --overwrite -C /opt
-  if [ $? != 0 ]; then
+  wget --tries=2 -O - "${ROON_PACKAGE_URI}" | tar -xvj --overwrite -C /opt
+  if [ $? -ne 0 ]; then
     echo "Error: Unable to install Roon Server."
     exit 1
   fi
 fi
 
-echo Verifying Roon installation
+echo "Verifying Roon installation"
 /opt/RoonServer/check.sh
 retval=$?
-if [ ${retval} != 0 ]; then
-  echo Verification of Roon installation failed.
+if [ ${retval} -ne 0 ]; then
+  echo "Verification of Roon installation failed."
   exit ${retval}
 fi
 
-# Start Roon
-#
-# Since we're invoking from a script, we need to
-# catch signals to terminate Roon nicely
+# Start Roon and forward signals
 /opt/RoonServer/start.sh &
 roon_start_pid=$!
 trap 'kill -INT ${roon_start_pid}' SIGINT SIGQUIT SIGTERM
-wait "${roon_start_pid}" # block until Roon terminates
+wait "${roon_start_pid}"
 retval=$?
 exit ${retval}
